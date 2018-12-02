3324d73a1a441506c6c3ef312cd2254e99390c03
After-today <35916378+After-today@users.noreply.github.com>
2018-12-02 15:11:05 +0800

    Create video_info.py

---
+++ video_info.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import pymongo
+
+with open('cookie_dict.txt', 'r') as f:
+    cookie_dict=json.load(f)
+
+client = pymongo.MongoClient(host='127.0.0.1', port=27017)
+db = client['test']
+v = db.video
+
+headers = {'header':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+for page in range(20):
+    url = 'https://api.bilibili.com/x/web-interface/newlist?rid=28&type=0&pn={}&ps=20&jsonp=jsonp&_=1543730016233'.format(page)
+    res = requests.get(url, headers=headers, cookies=cookie_dict)
+    archives = json.loads(res.text)['data']['archives']
+    for archive in archives:
+        result = v.insert(archive)
